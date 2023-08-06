b383e3b1358d6dff606567cea45e440de133b159
carteblanchecarriage <carteblanchecarriage@protonmail.com>
2023-08-06 16:42:35 -0400

    daisy test

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clhjiykm62wrg01t96ci97vy1/master',
+  uri: process.env.HYGRAPH_ENDPOINT,
   cache: new InMemoryCache(),
 });
 
--- pages/index.js
+++ pages/index.js
@@ -21,21 +21,45 @@ export default function Home({ recipes }) {
         <meta name='description' content='Recipes' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
+        <html data-theme='light'></html>
       </Head>
+      <button class='btn'>Button</button>
+      <div className='badge badge-outline'>default</div>
+      <div className='badge badge-primary badge-outline'>primary</div>
+      <div className='badge badge-secondary badge-outline'>secondary</div>
+      <div className='badge badge-accent badge-outline'>accent</div>
+      {/* You can open the modal using ID.showModal() method */}
+      <button className='btn' onClick={() => window.my_modal_3.showModal()}>
+        open modal
+      </button>
+      <dialog id='my_modal_3' className='modal'>
+        <form method='dialog' className='modal-box'>
+          <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
+            ✕
+          </button>
+          <h3 className='font-bold text-lg'>Hello!</h3>
+          <p className='py-4'>Press ESC key or click on ✕ button to close</p>
+        </form>
+      </dialog>
 
       <div className='flex flex-col justify-center items-center'>
         {recipes &&
           recipes.map((recipe) => (
             <>
               <div className='flex flex-col justify-center items-center mb-8 group hover:scale-105 w-[400px]'>
-                <Link key={recipe.id} href={`/recipes/${recipe.slug}`}>
+                <Link
+                  key={recipe.id}
+                  href={`/recipes/${recipe.slug}`}
+                  className='w-full'
+                >
                   <Image
                     src={recipe.photo.url}
-                    width={400}
+                    width={200}
                     height={200}
                     alt='food'
+                    className='h-[200px] w-full object-cover'
                   />
-                  <h1 className='text-2xl text-center p-2 w-[400px] border border-black'>
+                  <h1 className='text-2xl text-center p-2 w-full border border-black'>
                     {recipe.title}
                   </h1>
                 </Link>
